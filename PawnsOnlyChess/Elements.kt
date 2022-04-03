993ec934e865adc03f659f6872a0eaa1248e3c45
Akebu <74776297+akebu6@users.noreply.github.com>
2022-04-03 09:51:42 +0200

    created Elements class

---
+++ PawnsOnlyChess/Elements.kt
@@ -0,0 +1,8 @@
+enum class Elements(val view: String) {
+    WHITE("W"),
+    BLACK("B"),
+    EMPTY(" "),
+    LINE("  +---+---+---+---+---+---+---+---+"),
+    LETTERS("    a   b   c   d   e   f   g   h"),
+    REGEX_SYMBOLS("""[a-h][1-8][a-h][1-8]""")
+}
